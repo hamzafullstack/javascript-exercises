77ddf8d8fee3c2aaad1b3714ae930f880c744c98
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-09-17 14:21:44 +0500

    add javascript calculator exercise solution code

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
+};
+console.log(add(2, 1));
+
+const subtract = function(a, b) {
+  return a - b;
+};
+console.log(subtract(2, 1));
+
+const sum = function(...array) {
+	return [...array].reduce((total, item) => {
+    return total + item;
+  }, 0);
+};
+console.log(sum(1,3,4,5));
+
+const multiply = function(...array) {
+  return [...array].reduce((accumulator, val) => {
+    return accumulator * val;
+  }, 1);
 };
 
-const subtract = function() {
-	
-};
+console.log(multiply(2,3,4,5));
 
-const sum = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
+console.log(power(2, 3));
 
-const multiply = function() {
-
+const factorial = function(num) {
+  if(num < 0) {
+    return undefined; // factorial dont exist for negative numbers - GOOGLE - Web Dev - Docs.
+  }
+  let result = 1;
+  for(let index = 2; index <= num; index++) {
+    result *= index;
+  }
+  return result;
 };
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+console.log(factorial(5));
 
 // Do not edit below this line
 module.exports = {
